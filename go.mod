83e2ada720f8f762502fb883cff4d32ba767a696
EunHo Lee <ehlee8276@gmail.com>
2024-10-13 16:26:57 -0400

    start go project with makefile

---
+++ go.mod
@@ -0,0 +1,3 @@
+module dfs-project
+
+go 1.22.5
---
+++ p2p/transport.go
@@ -0,0 +1,9 @@
+package p2p
+
+// Peer is an interface that represent the remote node
+type Peer interface {
+}
+
+// Transport is anything that handle communication between nodes in the network
+type Transport interface {
+}
